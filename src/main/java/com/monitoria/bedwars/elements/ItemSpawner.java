c2350ecff3a9296ada62f6bab3c290ce2121b1f8
Bruno D'Luka <brunodlukaa@gmail.com>
2024-05-23 16:22:43 -0300

    fix: Correctly spawn items

--- src/main/java/com/monitoria/bedwars/elements/ItemSpawner.java
+++ src/main/java/com/monitoria/bedwars/elements/ItemSpawner.java
@@ -1,5 +1,6 @@
 package com.monitoria.bedwars.elements;
 
+import com.monitoria.bedwars.BedWars;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
@@ -7,6 +8,7 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Item;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ItemType;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -17,17 +19,18 @@ public class ItemSpawner {
 
     public Material itemType;
 
-    public Timer timer;
-
     public ItemSpawner(Location location, Material itemType) {
         this.location = location;
         this.itemType = itemType;
     }
 
     public void startTimer() {
-        timer = new Timer();
-        TimerTask task = new SpawnTask(this);
-        timer.schedule(task, 0, 1000);
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                spawn();
+            }
+        }.runTaskTimer(BedWars.instance, 0, 1000);
     }
 
 
@@ -36,31 +39,14 @@ public class ItemSpawner {
     public void spawn() {
         if (itemType == null) {
             if (switcher % 2 == 0) {
-                location.getWorld().dropItem(location, new ItemStack(Material.IRON_INGOT));
-                location.getWorld().dropItem(location, new ItemStack(Material.IRON_INGOT));
-                location.getWorld().dropItem(location, new ItemStack(Material.IRON_INGOT));
+                location.getWorld().dropItem(location, new ItemStack(Material.IRON_INGOT, 3));
             } else {
-                location.getWorld().dropItem(location, new ItemStack(Material.GOLD_INGOT));
-                location.getWorld().dropItem(location, new ItemStack(Material.GOLD_INGOT));
-                location.getWorld().dropItem(location, new ItemStack(Material.GOLD_INGOT));
+                location.getWorld().dropItem(location, new ItemStack(Material.GOLD_INGOT, 3));
             }
             switcher++;
             System.out.println("Spawnou item");
         } else {
             location.getWorld().dropItem(location, new ItemStack(itemType));
-        }
-    }
-
-    class SpawnTask extends TimerTask {
-        ItemSpawner itemSpawner;
-
-        SpawnTask(ItemSpawner spawner) {
-            this.itemSpawner = spawner;
-        }
-
-        public void run() {
-            System.out.println("Spawnando");
-            itemSpawner.spawn();
         }
     }
 
